91b1edf9e47ad3508e616bba05d3a5d69f810d20
RyanCharlotte <agorrineiro69@gmail.com>
2022-11-17 14:10:11 +0100

    Calculadora base

--- calculadora.java
+++ calculadora.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class calculadora {
     public static void main (String[]args){
+        Scanner teclado = new Scanner(System.in);
 
+        System.out.print("Primer número > ");
+        int n1 = teclado.nextInt();
+
+        System.out.print("Segundo número > ");
+        int n2 = teclado.nextInt();
+
+        System.out.print("Operador > ");
+        String operador = teclado.nextLine();
+
+        switch (operador){
+            default:
+                System.out.println("No es un operador válido");
+            break;
+        }
+
+        teclado.close();
     }
 }
