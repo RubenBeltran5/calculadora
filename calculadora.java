4d1aa4f375e19a9b17acdd20331644a9a25bffd6
“marcoslafoz” <marcos.lafoz96@gmail.com>
2022-11-18 13:24:44 +0100

    18/11/2022

--- calculadora.java
+++ calculadora.java
@@ -2,28 +2,28 @@ import java.util.Scanner;
 
 public class calculadora {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
+        try (Scanner teclado = new Scanner(System.in)) {
+            System.out.print("Primer número > ");
+            int n1 = teclado.nextInt();
 
-        System.out.print("Primer número > ");
-        int n1 = teclado.nextInt();
+            System.out.print("Segundo número > ");
+            int n2 = teclado.nextInt();
 
-        System.out.print("Segundo número > ");
-        int n2 = teclado.nextInt();
+            System.out.print("Operador > ");
+            String operador = teclado.nextLine();
 
-        System.out.print("Operador > ");
-        String operador = teclado.nextLine();
+            switch (operador) {
 
-        switch (operador) {
+                // Resta
+                case "-":
+                    int resultadoResta = n1 - n2;
+                    System.out.println(n1 + " - " + n2 + " = " + resultadoResta);
+                    break;
 
-            // Resta
-            case "-":
-                int resultadoResta = n1 - n2;
-                System.out.println(n1 + " - " + n2 + " = " + resultadoResta);
-                break;
-
-            default:
-                System.out.println("No es un operador válido");
-                break;
+                default:
+                    System.out.println("No es un operador válido");
+                    break;
+            }
         }
     }
 }
