c8294e813e5392e8ddb06616928bc8349aea8282
RubenBeltran5 <rubenbelgo@gmail.com>
2022-11-22 12:16:00 +0100

    Suma

--- calculadora.java
+++ calculadora.java
@@ -1,24 +1,32 @@
+$ cat calculadora.java
 import java.util.Scanner;
 
 public class calculadora {
-    public static void main (String[]args){
-        Scanner teclado = new Scanner(System.in);
+    public static void main(String[] args) {
+        try (Scanner teclado = new Scanner(System.in)) {
+            System.out.print("Primer número > ");
+            int n1 = teclado.nextInt();
 
-        System.out.print("Primer número > ");
-        int n1 = teclado.nextInt();
+            System.out.print("Segundo número > ");
+            int n2 = teclado.nextInt();
 
-        System.out.print("Segundo número > ");
-        int n2 = teclado.nextInt();
+            System.out.print("Operador > ");
+            String operador = teclado.nextLine();
 
-        System.out.print("Operador > ");
-        String operador = teclado.nextLine();
+            switch (operador) {
 
-        switch (operador){
-            default:
-                System.out.println("No es un operador válido");
-            break;
+                // Suma
+                case "+":
+                    int resultadoResta = n1 + n2;
+                    System.out.println(n1 + " + " + n2 + " = " + resultadoResta);
+                    break;
+
+                default:
+                    System.out.println("No es un operador válido");
+                    break;
+                    
+            }
         }
-
-        teclado.close();
     }
 }
+
